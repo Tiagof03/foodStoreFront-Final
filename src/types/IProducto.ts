bfdbaacb1ebb3985d1cb309872f09e969752d2dd
Sergio-Haquin <efrahaquin@gmail.com>
2025-10-30 23:09:54 -0300

    Interfaz de Categoria y Producto

---
+++ src/types/IProducto.ts
@@ -0,0 +1,19 @@
+import type { ICategoria } from "./ICategoria"
+// Interfaz utilizada a la hora de crear un producto
+export interface IProducto {
+
+    nombre:string,
+    //url de imagen a usar del producto
+    src:string,
+    precio:number,
+    idCategoria:number
+
+}
+
+// Interfaz utilizada para recibir los datos del backend
+export interface IProductoReturn {
+    nombre:string,
+    src:string,
+    precio:number,
+    categoria:ICategoria
+}
