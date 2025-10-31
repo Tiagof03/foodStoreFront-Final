fb027a779bb3c5116759cc3fb584b089822ae5dc
Tiagof03 <tiagohfunes03@gmail.com>
2025-10-30 23:27:18 -0300

    :)

--- src/types/ICategoria.ts
+++ src/types/ICategoria.ts
@@ -1,16 +1,11 @@
 // Interfaz utilizada para crear la categoria
 export interface ICategoria {
-
     nombre:string,
     descripcion:string
-
 }
-
 // Interfaz utilizada para recibir los datos del Backend
 export interface ICategoriaReturn {
-
     id:number
     nombre:string,
     descripcion:string
-
 }
--- src/types/IProducto.ts
+++ src/types/IProducto.ts
@@ -1,13 +1,11 @@
 import type { ICategoria } from "./ICategoria"
 // Interfaz utilizada a la hora de crear un producto
 export interface IProducto {
-
     nombre:string,
     //url de imagen a usar del producto
     src:string,
     precio:number,
     idCategoria:number
-
 }
 
 // Interfaz utilizada para recibir los datos del backend
